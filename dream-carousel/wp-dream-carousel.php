acf2e8ecf02daae3eaa3df5f071165c69822373f
J. Isaac Friend <isaac.friend@blueearth.net>
2014-02-05 17:04:32 -0600

    initial commit

---
+++ dream-carousel/wp-dream-carousel.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ *
+ * @package   WP_Dream_Carousel
+ * @author    J. Isaac Friend <j.isaac.friend@fueledbydreams.com>
+ * @license   GPL-2.0+
+ * @link      http://wpdreamcarousel.com
+ * @copyright 2014 J. Isaac Friend
+ *
+ * @wordpress-plugin
+ * Plugin Name:       WP Dream Carousel
+ * Plugin URI:        http://wpdreamcarousel.com
+ * Description:       A Carousel Plugin for WordPress using the ElastiSlide jQuery Plugin
+ * Version:           1.0.0
+ * Author:            J. Isaac Friend
+ * Author URI:        http://jisaacfriend.com
+ * Text Domain:       wp-dream-carousel
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Domain Path:       /languages
+ * GitHub Plugin URI: https://github.com/TaishiZiyi/wp_dream_carousel
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+/*----------------------------------------------------------------------------*
+ * Public-Facing Functionality
+ *----------------------------------------------------------------------------*/
+
+require_once( plugin_dir_path( __FILE__ ) . 'public/class-wp-dream-carousel.php' );
+
+/*
+ * Register hooks that are fired when the plugin is activated or deactivated.
+ * When the plugin is deleted, the uninstall.php file is loaded.
+ */
+register_activation_hook( __FILE__, array( 'WPDreamCarousel', 'activate' ) );
+register_deactivation_hook( __FILE__, array( 'WPDreamCarousel', 'deactivate' ) );
+
+
+add_action( 'plugins_loaded', array( 'WPDreamCarousel', 'get_instance' ) );
+
+/*----------------------------------------------------------------------------*
+ * Dashboard and Administrative Functionality
+ *----------------------------------------------------------------------------*/
+
+if ( is_admin() ) {
+
+	require_once( plugin_dir_path( __FILE__ ) . 'admin/class-wp-dream-carousel-admin.php' );
+	add_action( 'plugins_loaded', array( 'WPDreamCarousel_Admin', 'get_instance' ) );
+
+}
